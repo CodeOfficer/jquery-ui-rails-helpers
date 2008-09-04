4141a613857c27a582f81f179f6efe545e413c60
Russell Jones <spam@codeofficer.com>
2008-09-04 01:20:16 -0400

    big change: helper 'tabs_for' now requires a block

--- helpers/tabs_helper.rb
+++ helpers/tabs_helper.rb
@@ -1,32 +1,32 @@
 module TabsHelper
   
-  def tabs_for( *args, &proc )
+  def tabs_for( *options, &block )
     raise ArgumentError, "Missing block" unless block_given?
 
-    tabs = TabsHelper::TabsRenderer.new( &proc )
+    tabs = TabsHelper::TabsRenderer.new( *options, &block )
     tabs_html = tabs.render
-    concat tabs_html, proc.binding
+    concat tabs_html, block.binding
   end
   
   class TabsRenderer
     
-    def initialize( options={}, &proc )
+    def initialize( options={}, &block )
       raise ArgumentError, "Missing block" unless block_given?
 
-      @template = eval( 'self', proc.binding )
+      @template = eval( 'self', block.binding )
       @options = options
       @tabs = []
 
       yield self
     end
     
-    def create(tab_id, tab_text, options={}, &block)
+    def create( tab_id, tab_text, options={}, &block )
       raise "Block needed for TabsRenderer#CREATE" unless block_given?
-      @tabs << [tab_id, tab_text, options, block]
+      @tabs << [ tab_id, tab_text, options, block ]
     end
     
     def render
-      content_tag(:div, (render_tabs + render_bodies), {:id => :tabs}.merge(@options))
+      content_tag( :div, ( render_tabs + render_bodies ), { :id => :tabs }.merge( @options ) )
     end
     
     private #  ---------------------------------------------------------------------------
@@ -34,18 +34,18 @@ module TabsHelper
     def render_tabs
       content_tag :ul do
         @tabs.collect do |tab|
-          content_tag(:li, link_to(content_tag(:span, tab[1]), "##{tab[0]}") )
+          content_tag( :li, link_to( content_tag( :span, tab[1] ), "##{tab[0]}" ) )
         end
       end
     end
     
     def  render_bodies
       @tabs.collect do |tab| 
-        content_tag(:div, capture(&tab[3]), tab[2].merge(:id => tab[0])) 
+        content_tag( :div, capture( &tab[3] ), tab[2].merge( :id => tab[0] ) ) 
       end.to_s
     end
     
-    def method_missing(*args, &block)
+    def method_missing( *args, &block )
       @template.send( *args, &block )
     end
     
