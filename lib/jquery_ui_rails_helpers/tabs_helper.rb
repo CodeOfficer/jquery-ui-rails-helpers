9defee0bdbaa9346042cd58bdd46c115a712b0f7
Michael Hellein <michael@bearded.com>
2011-03-06 11:47:44 -0500

    simplified tabs helper - there was little need for so much abstraction

--- lib/jquery_ui_rails_helpers/tabs_helper.rb
+++ lib/jquery_ui_rails_helpers/tabs_helper.rb
@@ -1,8 +1,8 @@
 module TabsHelper
   include ActionView::Helpers::TagHelper,
-    ActionView::Helpers::UrlHelper,
-    ActionView::Helpers::CaptureHelper,
-    ActionView::Helpers::JavaScriptHelper
+          ActionView::Helpers::UrlHelper,
+          ActionView::Helpers::CaptureHelper,
+          ActionView::Helpers::JavaScriptHelper
 
   def tabs_for(opts={}, &block)
     opts = { :html => {}, :ui => {}, :script_for => :jquery_ui_helpers}.merge(opts)
@@ -19,41 +19,26 @@ module TabsHelper
   def create(tab_id, tab_text, opts={}, &block)
     content = capture(&block)
     opts = { :html => {} }.merge(opts)
-    @tabs << JqueryUiTab.new(tab_id, tab_text, opts[:html], content)
+    
+    @tabs << {
+      :tab => content_tag( :li, link_to( content_tag( :span, tab_text ), "#%s" % tab_id ) ),
+      :content => content_tag( :div, content, opts[:html].merge( :id => tab_id ) )
+    }
   end
 
   private
-
+  
     def to_s
-      tabs = ''
-      content = ''
-    
-      @tabs.each do |tab|
-        tabs << tab.render_tab
-        content << tab.render_content
-      end
+      # collect the html for our tabs
+      tabs = @tabs.collect {|t| t[:tab] }.join('')
+      content = @tabs.collect {|t| t[:content] }.join('')
       output = content_tag( :ul, tabs.html_safe ) + content.html_safe
       
+      # generate the javascript for jquery ui
       javascript = javascript_tag "$(function(){ $('#%s').tabs(); });" % @html_options[:id]
       content_for @script_for, javascript
       
+      # return the tabs html in a wrapper
       content_tag( :div, output.html_safe, @html_options)
-    end
-  
-    class JqueryUiTab
-      include ActionView::Helpers::TagHelper,
-        ActionView::Helpers::UrlHelper
-
-      def initialize(tab_id, tab_text, options, content)
-        @html_id, @text, @options, @content = tab_id, tab_text, options, content
-      end
-
-      def render_tab
-        content_tag( :li, link_to( content_tag( :span, @text ), "#%s" % @html_id ) )
-      end
-
-      def render_content
-        content_tag( :div, @content, @options.merge( :id => @html_id ) )
-      end
     end
 end
