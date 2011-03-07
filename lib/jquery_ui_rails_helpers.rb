a858d5ba32caa86af46c43d1a40a910aefd7f013
Michael Hellein <michael@bearded.com>
2011-03-06 23:01:19 -0500

    added dialog widget support

--- lib/jquery_ui_rails_helpers.rb
+++ lib/jquery_ui_rails_helpers.rb
@@ -1,9 +1,12 @@
 # JqueryUiRailsHelpers
 require "jquery_ui_rails_helpers/tabs_helper"
 require "jquery_ui_rails_helpers/accordion_helper"
+require "jquery_ui_rails_helpers/dialog_helper"
 require "rails"
 
 module JqueryUiRailsHelpers
 end
 
 ActionView::Base.send :include, TabsHelper
+ActionView::Base.send :include, AccordionHelper
+ActionView::Base.send :include, DialogHelper
---
+++ lib/jquery_ui_rails_helpers/dialog_helper.rb
@@ -0,0 +1,29 @@
+require "jquery_ui_rails_helpers/jquery_ui_base"
+
+module DialogHelper
+  include JqueryUiRailsHelpers::UiHelper
+
+  def ui_dialog(opts={}, &block)
+    ui(opts, JqueryUiDialog, &block)
+  end
+  
+  class JqueryUiDialog < JqueryUiRailsHelpers::JqueryUiBase
+    def initialize(opts={}, controller, &block)
+      @html_options = { :id => :dialog, :title => "" }.merge( opts[:html] )
+      @content = controller.capture(&block) || ""
+    end
+
+    def render
+      # collect the html for our tabs
+      @html = content_tag( :div, @content.html_safe, @html_options)
+  
+      # generate the javascript for jquery ui
+      @javascript = javascript_tag "$(function(){ $('#%s').dialog(); });" % @html_options[:id]
+
+      # return self, for chaining
+      self
+    end
+
+  end
+  
+end
